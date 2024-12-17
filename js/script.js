09e51a8b62be54a73182a77edbec90db08893f28
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-12-17 16:02:36 +0200

    wrote the slider for previews using slick slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// slider for hero section
 $(document).ready(function() {
     $('.my-slider1').slick({
         // dots: true, // Показать точки навигации
@@ -5,6 +6,19 @@ $(document).ready(function() {
         speed: 500, // Скорость анимации
         slidesToShow: 1, // Количество слайдов одновременно
         slidesToScroll: 1, // Количество прокручиваемых слайдов
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+// slider for reviews section
+$(document).ready(function() {
+    $('.my-slider2').slick({
+        // dots: true, 
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         fade: true,
         cssEase: 'linear'
     });
